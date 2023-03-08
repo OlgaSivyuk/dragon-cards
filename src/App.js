291e763ada34d286459e6343492f7a3528ab180b
o.sivyuk <o.sivyuk@geesoft.ru>
2023-03-08 17:43:44 +0400

    create basic card structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,106 @@
-import './App.css';
+import design from './images/design.png';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div className="dragon-card-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Путь героя воглера</header>
+          <h1>Название главы</h1>
+          <ul>
+            <li>
+              <img src={design} alt="" />
+              first
+            </li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Путь героя воглера</header>
+          <h1>Название главы</h1>
+          <ul>
+            <li>
+              <img src={design} alt="" />
+              first
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Путь героя воглера</header>
+          <h1>Название главы</h1>
+          <ul>
+            <li>
+              <img src={design} alt="" />
+              first
+            </li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Путь героя воглера</header>
+          <h1>Название главы</h1>
+          <ul>
+            <li>
+              <img src={design} alt="" />
+              first
+            </li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Путь героя воглера</header>
+          <h1>Название главы</h1>
+          <ul>
+            <li>
+              <img src={design} alt="" />
+              first
+            </li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Путь героя воглера</header>
+          <h1>Название главы</h1>
+          <ul>
+            <li>
+              <img src={design} alt="" />
+              first
+            </li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+      </div>
     </div>
   );
 }
